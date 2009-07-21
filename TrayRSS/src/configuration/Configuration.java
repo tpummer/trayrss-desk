1fb2835499d0eecd48458bf85c22819f7753ae57
Thomas Pummer <dev@nullpointer.at>
2009-07-21 20:46:36 +0000

    + Storing properties + Language Selection

--- TrayRSS/src/configuration/Configuration.java
+++ TrayRSS/src/configuration/Configuration.java
@@ -1,5 +1,11 @@
 package configuration;
 
+import gui.tray.ConfigFrameCaptions;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Properties;
 
 public class Configuration {
@@ -17,6 +23,17 @@ public class Configuration {
 	}
 	
 	public void save(){
-		
+		props.setProperty("trayrss.lang", ReferenceCollection.LANGUAGE);
+		props.setProperty("trayrss.displaycout", ""+ReferenceCollection.DISPLAY_COUNT);
+		props.setProperty("trayrss.displaysecond", ""+ReferenceCollection.DISPLAY_SECONDS);
+		try {
+			props.storeToXML(new FileOutputStream(ReferenceCollection.CONFIG),"TrayRSS Configuration");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
