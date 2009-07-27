48fa09092320c65520283225e92bbf5171be7fe5
Thomas Pummer <dev@nullpointer.at>
2009-07-27 11:24:31 +0000

    * equals and hash of Feed and News * link between Feed and News

--- src/at/nullpointer/trayrss/configuration/feeds/db/News.java
+++ src/at/nullpointer/trayrss/configuration/feeds/db/News.java
@@ -7,9 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.Hibernate;
 
 
 @Entity
@@ -22,6 +26,7 @@ public class News {
 	private Date publishedDate;
 	private Date updatedDate;
 	private String uri;
+	private Feed feed;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -74,5 +79,53 @@ public class News {
 		this.uri = uri;
 	}
 	
+	@ManyToOne
+	@JoinColumn(name = "feed_id")
+	public Feed getFeed(){
+		return feed;
+	}
+	public void setFeed(Feed feed){
+		this.feed = feed;
+	}
+		
+	@Override
+	public boolean equals(Object o){
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(0)) return false;
+		
+		News news = (News) o;
+		
+		if(this.author != null && this.author.equals(news.getAuthor())){
+		} else return false;
+		
+		if(this.title != null && this.title.equals(news.getTitle())){
+		} else return false;
+		
+		if(this.publishedDate != null && this.publishedDate.equals(news.getPublishedDate())){
+		} else return false;
+		
+		if(this.updatedDate != null && this.updatedDate.equals(news.getUpdatedDate())){
+		} else return false;
+		
+		if(this.uri != null && this.uri.equals(news.getUri())){
+		} else return false;
+		
+		if(this.feed != null && this.feed.equals(news.getFeed())){
+		} else return false;
+		
+		return true;
+	}
 	
+	@Override
+	public int hashCode() {
+		
+		int result;
+		result = (author != null ? author.hashCode() : 0);
+		result = 29 * result + (title != null ? title.hashCode() : 0);
+		result = 29 * result + (publishedDate != null ? publishedDate.hashCode() : 0);
+		result = 29 * result + (updatedDate != null ? updatedDate.hashCode() : 0);
+		result = 29 * result + (uri != null ? uri.hashCode() : 0); 
+		result = 29 * result + (feed != null ? feed.hashCode() : 0);
+		return result;	
+	}
 }
