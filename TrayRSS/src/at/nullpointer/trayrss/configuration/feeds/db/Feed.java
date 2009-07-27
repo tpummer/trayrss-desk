2d4625283e957a1cb019fb443d2622be04b1c6f7
Thomas Pummer <dev@nullpointer.at>
2009-07-27 11:24:31 +0000

    * equals and hash of Feed and News * link between Feed and News

--- TrayRSS/src/at/nullpointer/trayrss/configuration/feeds/db/Feed.java
+++ TrayRSS/src/at/nullpointer/trayrss/configuration/feeds/db/Feed.java
@@ -20,16 +20,21 @@ package at.nullpointer.trayrss.configuration.feeds.db;
  */
 import java.io.Serializable;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.Hibernate;
 
 /**
  * Represents a RSS or Atom Feed
@@ -39,7 +44,7 @@ import javax.persistence.TemporalType;
  */
 
 @Entity
-@Table(name = "feeds")
+@Table(name = "feed")
 public class Feed implements Serializable {
 
 	
@@ -53,6 +58,7 @@ public class Feed implements Serializable {
 	private Long intervall;
 	private Date lastAction;
 	private String name;
+	private List<News> news = new ArrayList<News>();
 	
 	
 	@Id
@@ -97,6 +103,50 @@ public class Feed implements Serializable {
 		this.name = name;
 	}
 	
+	@OneToMany(mappedBy = "feed")
+	public List<News> getNews(){
+		return news;
+	}
 	
+	public void setNews(List<News> news){
+		this.news = news;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(0)) return false;
+		
+		Feed feed = (Feed) o;
+		
+		if(this.url != null){
+			if(!this.url.equals(feed.getUrl())) return false;
+		}
+		if(this.name != null){
+			if(!this.name.equals(feed.getName())) return false;
+		}
+		if(this.lastAction != null){
+			if(!this.lastAction.equals(feed.getLastAction())) return false;
+		}
+		if(this.intervall != null){
+			if(!this.intervall.equals(feed.getIntervall())) return false;
+		}
+		if(this.news != null){
+			if(!this.news.equals(feed.getNews())) return false;
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		int result;
+		result = (url != null ? url.hashCode() : 0);
+		result = 29 * result + (name != null ? name.hashCode() : 0);
+		result = 29 * result + (lastAction != null ? lastAction.hashCode() : 0);
+		result = 29 * result + (intervall != null ? intervall.hashCode() : 0);
+		result = 29 * result + (news != null ? news.hashCode() : 0); 
+		return result;	
+	}
 
 }
