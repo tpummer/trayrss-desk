ea2411eaad1da7d2ca87531f3961c64e2a85d989
Thomas Pummer <dev@nullpointer.at>
2009-08-19 20:33:36 +0000

    prepare monitor - still not working

---
+++ TrayRSS/src/at/nullpointer/trayrss/test/monitor/MonitorTest.java
@@ -0,0 +1,14 @@
+package at.nullpointer.trayrss.test.monitor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MonitorTest {
+
+	@Test
+	public void testMonitor() {
+		fail("Not yet implemented");
+	}
+
+}
