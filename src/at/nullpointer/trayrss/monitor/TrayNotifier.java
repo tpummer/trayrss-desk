6c880faa1a4e24877b564e458a71927c19548486
Thomas Pummer <dev@nullpointer.at>
2009-12-15 09:52:58 +0000

    * new popup at notification opens the entry in a browser window * Added JNotification Lib * notification is now available

--- src/at/nullpointer/trayrss/monitor/TrayNotifier.java
+++ src/at/nullpointer/trayrss/monitor/TrayNotifier.java
@@ -19,47 +19,187 @@
  */
 package at.nullpointer.trayrss.monitor;
 
+import java.awt.Component;
+import java.awt.Desktop;
+import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+import de.jutzig.jnotification.Corner;
+import de.jutzig.jnotification.JNotificationPopup;
+import de.jutzig.jnotification.PopupManager;
+import de.jutzig.jnotification.animation.FadeIn;
 
 import at.nullpointer.trayrss.configuration.ReferenceCollection;
 import at.nullpointer.trayrss.configuration.feeds.db.Feed;
 import at.nullpointer.trayrss.configuration.feeds.db.News;
 
-public class TrayNotifier {
-	
+public class TrayNotifier implements Runnable {
+
 	ArrayList<Notification> input = new ArrayList<Notification>();
-	
-	public void notify(News news, Feed feed){
-		ReferenceCollection.LOG.debug("TrayNotifier: notify");
-		
-		String title = input.get(0).getNews().getTitle();
-		String name = input.get(0).getFeed().getName();
-		
-		ReferenceCollection.TRAY_ICON.displayMessage(title, name, TrayIcon.MessageType.INFO);
-		
-		input.remove(0);
-		
-		ReferenceCollection.LOG.debug("-----------------------------------------------");
+	private static PopupManager popman;
+	private static JButton bread;
+	private static JButton bstop;
+	private static JButton bclose;
+
+	public void notifyNews() {
+		ReferenceCollection.LOG
+		.debug("--------------TrayNotifier: notify-------------------");
+
+		if (getSize() > 0) {
+			String title = input.get(0).getNews().getTitle();
+			String name = input.get(0).getFeed().getName();
+			String url = input.get(0).getNews().getUri();
+			
+			JNotificationPopup popup = null;
+			
+			bread = new JButton("Read");
+			
+			bstop = new JButton("Stop");
+			
+			bclose = new JButton("Close");
+			
+			popup = new JNotificationPopup(createComponent(title, name, url));
+			popup.setAnimator(new FadeIn(popup,2000));
+			
+			bread.addActionListener(new BrowserButton(popup,popman,url));
+			bstop.addActionListener(new Dispose(popup,popman));
+			bclose.addActionListener(new Dispose(popup,popman));
+			
+			popman.enqueuePopup(popup);
+
+			input.remove(0);
+		} else {
+			ReferenceCollection.LOG.debug("Nothing found to notify!");
+		}
+
+		ReferenceCollection.LOG
+				.debug("----------------End-------------------------");
 	}
-	
-	public void addToNotify(News news, Feed feed){
+
+	public void addToNotify(News news, Feed feed) {
 		ReferenceCollection.LOG.debug("TrayNotifier: addToNotify");
-		
+
 		Notification notifi = new Notification();
 		notifi.setFeed(feed);
 		notifi.setNews(news);
-		
+
 		input.add(notifi);
-		ReferenceCollection.LOG.debug("TrayNotifier: " + notifi.getFeed().getName());
-		ReferenceCollection.LOG.debug("TrayNotifier: " + notifi.getNews().getTitle());
-		ReferenceCollection.LOG.debug("-----------------------------------------------");
+		ReferenceCollection.LOG.debug("TrayNotifier: "
+				+ notifi.getFeed().getName());
+		ReferenceCollection.LOG.debug("TrayNotifier: "
+				+ notifi.getNews().getTitle());
+		ReferenceCollection.LOG.debug(getSize());
+		ReferenceCollection.LOG
+				.debug("-----------------------------------------------");
 	}
-	
-	public int getSize(){
+
+	public int getSize() {
 		return input.size();
 	}
 
+	@Override
+	public void run() {
+		
+		popman = new PopupManager(ReferenceCollection.DISPLAY_SECONDS*1000, Corner.LOWER_RIGHT, new Point(30,100));
+		
+		while (true) {
+			this.notifyNews();
+			try {
+				popman = new PopupManager(ReferenceCollection.DISPLAY_SECONDS*1000, Corner.LOWER_RIGHT, new Point(30,100));
+				Thread.sleep(ReferenceCollection.DISPLAY_SECONDS*1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	
+	private static Component createComponent(String title, String feedName, String url)
+	{
+		JPanel panel = new JPanel(new GridLayout(2,1));
+		JLabel ltitel =new JLabel();
+		ltitel.setText("<html><b>"+title+"</b></html>");
+		panel.add(ltitel);
+		
+		JPanel buttonPanel = new JPanel(new GridLayout(1,3));
+		
+		buttonPanel.add(bread);
+		buttonPanel.add(bstop);
+		buttonPanel.add(bclose);
+		
+		panel.add(buttonPanel);
+		
+		panel.setBorder(new TitledBorder(feedName));
+		return panel;
+		
+	}
+
+}
+
+class Dispose implements ActionListener{
+
+	JNotificationPopup popup;
+	PopupManager manager;
+	
+	public Dispose(Component popup, PopupManager manager) {
+		super();
+		this.popup = (JNotificationPopup) popup;
+		this.manager=manager;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		manager.dequeuePopup(popup);
+		
+	}
+	
+}
+
+class BrowserButton implements ActionListener{
+
+	JNotificationPopup popup;
+	PopupManager manager;
+	String url;
+	
+	public BrowserButton(Component popup, PopupManager manager, String url) {
+		super();
+		this.popup = (JNotificationPopup) popup;
+		this.manager=manager;
+		this.url = url;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		URI uri;
+		try {
+			uri = new URI(url);
+			Desktop.getDesktop ().browse (uri);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		manager.dequeuePopup(popup);
+		
+	}
+	
 }
