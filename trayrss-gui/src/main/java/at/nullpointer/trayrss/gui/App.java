4d4993cf95d86bfc9f2e680ba9813a527282b854
Thomas Pummer <dev@nullpointer.at>
2011-05-16 17:42:03 +0000

    establishing multi module project new gui project

---
+++ trayrss-gui/src/main/java/at/nullpointer/trayrss/gui/App.java
@@ -0,0 +1,13 @@
+package at.nullpointer.trayrss.gui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
