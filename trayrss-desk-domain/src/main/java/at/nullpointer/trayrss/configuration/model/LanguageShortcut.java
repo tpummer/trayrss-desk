c9caa26e2b7a6781a98d7fd909861e860937c3a4
Thomas Pummer <dev@nullpointer.at>
2013-08-15 00:53:12 +0200

    Added Assert statements to test

--- trayrss-desk-domain/src/main/java/at/nullpointer/trayrss/configuration/model/LanguageShortcut.java
+++ trayrss-desk-domain/src/main/java/at/nullpointer/trayrss/configuration/model/LanguageShortcut.java
@@ -14,12 +14,10 @@
  */
 package at.nullpointer.trayrss.configuration.model;
 
-import lombok.Getter;
 
 public enum LanguageShortcut {
     DE( "de" ), EN( "en" );
 
-    @Getter
     private final String SHORTCUT;
 
 
